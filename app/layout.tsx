174055e9595b4615d2ac152d91d6a3131e2ec066
Saumik <aidenpearcesaumik@gmail.com>
2025-08-28 06:49:44 +0600

    thumbnail added

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,14 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Zappy",
   description: "Your personal Game, Anime, and Movie recommender.",
+  // Add the following lines:
+  openGraph: {
+    images: ['/zappy.png'], // Replace with the actual path to your image
+  },
+  twitter: {
+    card: "summary_large_image", // Important for Twitter to display the image
+    images: ['/zappy.png'], // Replace with the actual path to your image
+  },
 };
 
 export default function RootLayout({
